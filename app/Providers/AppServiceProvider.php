d4182d9793b800c1e934d1f341bb19cdbce41bf2
Daniel Abernathy <dabernathy89@gmail.com>
2016-04-21 10:53:52 -0500

    Step 5: Register the TwitterStream Class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Phirehose;
+use App\TwitterStream;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('App\TwitterStream', function ($app) {
+            $twitter_access_token = env('TWITTER_ACCESS_TOKEN', null);
+            $twitter_access_token_secret = env('TWITTER_ACCESS_TOKEN_SECRET', null);
+            return new TwitterStream($twitter_access_token, $twitter_access_token_secret, Phirehose::METHOD_FILTER);
+        });
     }
 }
