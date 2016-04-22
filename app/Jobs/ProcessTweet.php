a363bfab8232876c77482276dbbb7874c4b2f207
Daniel Abernathy <dabernathy89@gmail.com>
2016-04-22 16:05:46 -0500

    Step 11: Make the ProcessTweet Job Useful

--- app/Jobs/ProcessTweet.php
+++ app/Jobs/ProcessTweet.php
@@ -6,6 +6,7 @@ use App\Jobs\Job;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Tweet;
 
 class ProcessTweet extends Job implements ShouldQueue
 {
@@ -31,7 +32,22 @@ class ProcessTweet extends Job implements ShouldQueue
     public function handle()
     {
         $tweet = json_decode($this->tweet,true);
-        var_dump($tweet['text']) . PHP_EOL;
-        var_dump($tweet['id_str']) . PHP_EOL;
+        $tweet_text = isset($tweet['text']) ? $tweet['text'] : null;
+        $user_id = isset($tweet['user']['id_str']) ? $tweet['user']['id_str'] : null;
+        $user_screen_name = isset($tweet['user']['screen_name']) ? $tweet['user']['screen_name'] : null;
+        $user_avatar_url = isset($tweet['user']['profile_image_url_https']) ? $tweet['user']['profile_image_url_https'] : null;
+
+        if (isset($tweet['id'])) {
+            Tweet::create([
+                'id' => $tweet['id_str'],
+                'json' => $this->tweet,
+                'tweet_text' => $tweet_text,
+                'user_id' => $user_id,
+                'user_screen_name' => $user_screen_name,
+                'user_avatar_url' => $user_avatar_url,
+                'approved' => 0
+            ]);
+        }
+
     }
 }
